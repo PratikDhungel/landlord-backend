038f97244fe21f12b69fda2ce0ac8106abc02c88
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-07-31 10:03:49 +0545

    Update rental query to return owner, tenant and plan name

--- src/models/rentals.models.js
+++ src/models/rentals.models.js
@@ -11,7 +11,17 @@ async function createRental({ planId, ownerId, tenantId, startDate }) {
 }
 
 async function findRentalsByOwner({ ownerId }) {
-  const res = await db.query('SELECT * FROM rentals WHERE owner_id = $1', [ownerId])
+  const query = `
+  SELECT re.id, ow.id user_id, ow.first_name owner_first_name, ow.last_name owner_last_name, ow.email owner_email,
+  tn.id tenant_id, tn.first_name tenant_first_name, tn.last_name tenant_last_name, tn.email tenant_email,
+  rp.id plan_id, rp.name plan_name from rentals as re
+  LEFT JOIN users as ow on re.owner_id = ow.id
+  LEFT JOIN users as tn on re.tenant_id = tn.id
+  LEFT JOIN rental_plans as rp on re.plan_id = rp.id
+  WHERE ow.id = $1;
+  `
+
+  const res = await db.query(query, [ownerId])
 
   return res.rows
 }
