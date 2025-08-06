26e7934394d4842cab34e2530b4735b153ff06e7
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-06 17:15:27 +0545

    Rename route for liable rentals

--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -11,7 +11,7 @@ router.post('/create-rental-plan', authenticateJWT, rentalPlansControllers.creat
 
 router.post('/create-rental', authenticateJWT, rentalsControllers.createRental)
 router.get('/owned-rentals', authenticateJWT, rentalsControllers.getRentalsForOwner)
-router.get('/rented', authenticateJWT, rentalsControllers.getRentalsForTenants)
+router.get('/liable-rentals', authenticateJWT, rentalsControllers.getRentalsForTenants)
 
 router.post('/create-rental-payment', authenticateJWT, rentalPaymentsControllers.recordPaymentForRental)
 
