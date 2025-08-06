73ea91b760a4ef8a4379b9bec911b69a8ece5ea2
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-06 21:52:02 +0545

    Update query to fetch liable rentals for user

--- src/models/rentals.models.js
+++ src/models/rentals.models.js
@@ -27,7 +27,17 @@ async function findRentalsByOwner({ ownerId }) {
 }
 
 async function findRentalsByTenant({ tenantId }) {
-  const res = await db.query('SELECT * FROM rentals WHERE tenant_id = $1', [tenantId])
+  const query = `
+  SELECT re.id, re.start_date start_date, ow.id user_id, ow.first_name owner_first_name, ow.last_name owner_last_name, ow.email owner_email,
+  tn.id tenant_id, tn.first_name tenant_first_name, tn.last_name tenant_last_name, tn.email tenant_email,
+  rp.id plan_id, rp.name plan_name from rentals as re
+  LEFT JOIN users as ow on re.owner_id = ow.id
+  LEFT JOIN users as tn on re.tenant_id = tn.id
+  LEFT JOIN rental_plans as rp on re.plan_id = rp.id
+  WHERE tn.id = $1;
+  `
+
+  const res = await db.query(query, [tenantId])
 
   return res.rows
 }
