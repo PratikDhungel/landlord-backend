b685c9ed6d8d6f4b795746226944adb484341148
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-07 11:28:16 +0545

    Add type parser for postgres to convert amount from string to number

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,21 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+require('dotenv').config()
+
+const { Pool, types } = require('pg')
+
+types.setTypeParser(types.builtins.NUMERIC, (value) => {
+  if (value === null) {
+    return null
+  }
+
+  // Parsing numeric values to float from string
+  // NOTE Assumption is NUM(10, 2) i.e. 2 decimal precision is being used
+  return parseFloat(value)
+})
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-});
+})
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
-};
+}
