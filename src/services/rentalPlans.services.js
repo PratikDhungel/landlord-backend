665977fd09174f021ffb5a1861eccbe7617b1cd2
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-07 16:42:37 +0545

    Remove custom parsing for rate value returned in rental plan services

--- src/services/rentalPlans.services.js
+++ src/services/rentalPlans.services.js
@@ -1,5 +1,4 @@
 const rentalPlansModels = require('../models/rentalPlans.models')
-const { parsePGNumericData } = require('../utils/dataParser')
 
 async function createRentalPlan(rentalPlanPayload) {
   const rentalPlan = await rentalPlansModels.createRentalPlan(rentalPlanPayload)
@@ -10,13 +9,7 @@ async function createRentalPlan(rentalPlanPayload) {
 async function getRentalPlans({ ownerId }) {
   const rentalPlans = await rentalPlansModels.findRentalPlansByUser({ ownerId })
 
-  const mappedRentalPlans = rentalPlans.map((eachRentalPlan) => {
-    const parsedRateValue = parsePGNumericData(eachRentalPlan.rate)
-
-    return { ...eachRentalPlan, rate: parsedRateValue }
-  })
-
-  return mappedRentalPlans
+  return rentalPlans
 }
 
 module.exports = { createRentalPlan, getRentalPlans }
--- src/utils/dataParser.js
+++
@@ -1,9 +0,0 @@
-function parsePGNumericData(stringValue) {
-  if (typeof stringValue === 'string') {
-    return Number(stringValue)
-  }
-
-  return null
-}
-
-module.exports = { parsePGNumericData }
