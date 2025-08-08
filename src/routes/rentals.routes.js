ddd51b8f65e97a9c981117f4efa3c3f55263a685
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-08-08 12:30:49 +0545

    Rename route to fetch rental details

--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -12,7 +12,7 @@ router.post('/create-rental-plan', authenticateJWT, rentalPlansControllers.creat
 router.post('/create-rental', authenticateJWT, rentalsControllers.createRental)
 router.get('/owned-rentals', authenticateJWT, rentalsControllers.getRentalsForOwner)
 router.get('/liable-rentals', authenticateJWT, rentalsControllers.getRentalsForTenants)
-router.get('/liable-rental/:id', authenticateJWT, rentalsControllers.getLiableRentalDetailById)
+router.get('/details/:id', authenticateJWT, rentalsControllers.getLiableRentalDetailById)
 
 router.post('/create-rental-payment', authenticateJWT, rentalPaymentsControllers.recordPaymentForRental)
 
